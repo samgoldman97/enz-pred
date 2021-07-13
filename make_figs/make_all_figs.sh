27e2ba404fdb6aa95d56407628ac392c9ab2cde3
Sam Goldman <samlg@mit.edu>
2021-07-12 22:50:25 -0400

    Update qsar and psar for multi

--- make_figs/make_all_figs.sh
+++ make_figs/make_all_figs.sh
@@ -3,4 +3,4 @@ python make_figs/make_active_site_plots.py
 python make_figs/make_benchmarking_plots_psar.py
 python make_figs/make_benchmarking_plots_qsar.py
 
-#python make_figs/make_heatmaps.py
+python make_figs/make_heatmaps.py
